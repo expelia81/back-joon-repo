dfcb8b98a347e157a70bb5b1c37da0ee51ed17ef
김창훈 <expelia81@gmail.com>
2024-04-18 23:41:53 +0900

    2082 풀었는데, 답이 아니다...? 예제는 3개 다 통과했는데 뭐꼬...

--- problems_2404/pro_2082/Main.java
+++ problems_2404/pro_2082/Main.java
@@ -13,32 +13,54 @@ public class Main {
 	public static List<char[][]> numbers = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
+		createNumbers();
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		for (int i = 0; i < 5; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine(), "\n");
 			String s = st.nextToken();
-			for (int j = 0; i < 3; i++) {
-				first[i][j]=s.charAt(i);
-				second[i][j]=s.charAt(4+i);
-				third[i][j]=s.charAt(8+i);
-				fourth[i][j]=s.charAt(12+i);
+			for (int j = 0; j < 3; j++) {
+				first[i][j]=s.charAt(j);
+				second[i][j]=s.charAt(4+j);
+				third[i][j]=s.charAt(8+j);
+				fourth[i][j]=s.charAt(12+j);
 			}
 		}
+
+		bw.write(String.valueOf(findNumber(first)));
+		bw.write(String.valueOf(findNumber(second)));
+		bw.write(":");
+		bw.write(String.valueOf(findNumber(third)));
+		bw.write(String.valueOf(findNumber(fourth)));
 
 		bw.flush();
 		br.close();
 		bw.close();
 	}
+	static int findNumber(char[][] target) {
+		for (int i = 0; i < 9; i++) {
+			if (check(target, numbers.get(i))) {
+				System.out.println(i);
+				return i;
+			}
+		}// 굳이 9번째 순회를 돌 필요는 없음.
+		return 9;
+	}
 	static boolean check(char[][] target, char[][] number) {
 		for (int i = 0; i <5; i++) {
 			for (int j = 0; j < 3; j++) {
-				char a = target[]
-				if ()
+				char a = target[i][j];
+				char b = number[i][j];
+					// target은 꺼져있지만 number는 켜져있는 건 상관없다.
+					// number는 꺼져있지만 target은 켜져있다면 안된다.
+					if (a=='#' && b=='.') {
+						return false;
+					}
 			}
 		}
+		return true;
 	}
 	public static void register(String s, int number) {
 		for (int i = 0; i < 10; i++) {
