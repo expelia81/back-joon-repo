79ad45ff55a2b43601abc760096d2015095c6a21
김창훈 <ch.kim@okestro.com>
2024-05-03 22:51:18 +0900

    commit

--- problems_2404/pro_2247_실질적약수/Main.java
+++ problems_2404/pro_2247_실질적약수/Main.java
@@ -5,62 +5,51 @@ import java.util.*;
 
 public class Main {
 
-	public static Set<Integer> prm = new HashSet<>();
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
+		/*
+			약수란 무엇인가.
+			2부터 순회하며, 자신에게 도달할 때까지 곱해보면 된다.
 
-		findIDontKnow(n);
+			1인 경우는 제외한다. (실질적 약수라 했으므로)
 
+			2->4->6->8
+			여기까지 도달하는데 필요한 배율을 곱해나간다.
+			그러면,
+			4=2
+			6=3
+			8=4
+			또, 3도 똑같이 배율을 곱해나간다.
+			3->6->9->12....
+			그러면,
+			6=2
+			9=3
+			12=4
+
+			이렇게 해나가보면 된다.
+			즉, 자기 자신 제외하고, 자신의 n배수로 곱해가며 최대값 n이 될 때까지 반복해나가면된다.
+		 */
+//		long[] arr = new long[n+1];
 		long result = 0;
-		for (int i = 1; i <= n; i++) {
-			if (prm.contains(i)) {
-				continue;
+
+		// 생각해보니, 등차수열 n회 반복하면 되잖아,.,.
+		for (long i = 2; i <= n; i++) {
+			long b = n/i;
+			long a = (b*(b+1))/2;
+			if (n>1){
+				result += a-1;
 			}
-			result += sod(i);
 		}
 
 		bw.write(String.valueOf(result%1000000));
-		bw.newLine();
-
 
 		bw.flush();
 		br.close();
 		bw.close();
-	}
-
-	private static long sod(int i) {
-		long result = 0;
-//		int max = i;
-		for (int j = 2; j <= (int) Math.sqrt(i); j++) {
-			if (i%j==0) {
-				int r = i/j;
-				if (i!=j && j !=r) {
-					result+= j;
-					result += r;
-				} else if (i!=j) {
-					result+= j;
-				}
-			}
-//			max = i/j;
-		}
-//		System.out.println("sod {}: ".replace("{}",i+"")+result);
-		return result;
-	}
-
-	public static void findIDontKnow(int max) {
-		boolean[] arr = new boolean[max+1];
-		for (int i = 2;  i<=max ; i++) {
-			if (!arr[i]) {
-				prm.add(i);
-			}
-			for (int j=i; j<=max; j+=i) {
-				arr[j]=true;
-			}
-		}
 	}
 
 
