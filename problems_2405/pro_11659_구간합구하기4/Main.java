d8afc83460d6e1acabd14272c57b83219e9e161d
김창훈 <expelia81@gmail.com>
2024-06-01 19:55:13 +0900

    commit

--- problems_2405/pro_11659_구간합구하기4/Main.java
+++ problems_2405/pro_11659_구간합구하기4/Main.java
@@ -1,9 +1,24 @@
 package problems_2405.pro_11659_구간합구하기4;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
+
+	public static class Tree {
+		public int index;
+		public long sum;
+		public Tree right;
+
+		public Tree(long sum, int index, Tree node) {
+			this.index = index;
+			this.sum = sum;
+			right = node;
+		}
+	}
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,24 +28,43 @@ public class Main {
 
 		/* 여러 정수 쓰는 경우 */
 		int n = Integer.parseInt(st.nextToken());
-		int size = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 
 		/* 배열 필요한 경우 */
 		int[] arr = new int[n];
-		int[] sum = new int[n];
+		long[] sum = new long[n];
 		st = new StringTokenizer(br.readLine(), " ");
+		Map<Long, Tree> map = new HashMap<>();
 		for (int j = 0; j < n; j++) {
 			arr[j]=Integer.parseInt(st.nextToken());
 			sum[j] = j==0 ? arr[j] : sum[j-1]+arr[j];
+			map.put(sum[j], new Tree(sum[j], j, map.get(sum[j])));
 		}
-		for (int i = 0; i < size; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int startSum = start==1 ? 0 : sum[start-2];
-			int endSum = sum[end-1];
-			bw.write(String.valueOf(endSum - startSum)+"\n");
+		long result = 0;
+		if (n==1) {
+			bw.write(k==arr[0] ? "1":"0");
+			bw.flush();
+			bw.close();
+			return;
 		}
+
+		for (long v : map.keySet()) {
+			Tree node = map.get(v);
+			int count = 1;
+
+			while (node.right!=null) {
+				count++;
+				node = node.right;
+			}
+			System.out.println(v +" 번째 노드 개수 : " + count);
+		}
+
+		//부분합.
+		// f(i,j)=sum(i)-sum(j-1)=k;
+
+
+
+		bw.write(result+"");
 
 
 
