edb9031a85eb084ee49d9af99a7ac25b8f37d76d
김창훈 <expelia81@gmail.com>
2024-06-08 21:32:25 +0900

    commit 토마토!

--- problems_2406/pro_7576_토마토/Main.java
+++ problems_2406/pro_7576_토마토/Main.java
@@ -1,10 +1,32 @@
 package problems_2406.pro_7576_토마토;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int[][] tomatoes;
+	static Queue<Tomato> queue = new LinkedList<>();
+	public static int[] dx = {1, -1, 0, 0};
+	public static int[] dy = {0, 0, 1, -1};
+	public static class Tomato {
+		int x;
+		int y;
+		int day;
+
+		public Tomato(int x, int y) {
+			this.x = x;
+			this.y = y;
+			day=0;
+		}
+		public Tomato(int x, int y, int day) {
+			this.x = x;
+			this.y = y;
+			tomatoes[y][x]=1;
+			this.day=day;
+		}
+	}
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,44 +39,34 @@ public class Main {
 		int y = Integer.parseInt(st.nextToken());
 
 		/* 배열 필요한 경우 */
-		int[][] tomatoes = new int[y][x];
-		int[][] resultTomatoes = new int[y][x];
-		int[][] days = new int[y][x];
-
+		tomatoes = new int[y][x];
 
 
 		for (int i = 0; i < y; i++) {
 			st = new StringTokenizer(br.readLine()," ");
-			Arrays.fill(days[i], Integer.MAX_VALUE);
 			for (int j = 0; j < x; j++) {
 				tomatoes[i][j]=Integer.parseInt(st.nextToken());
-				resultTomatoes[i][j]=tomatoes[i][j];
-			}
-		}
-		for (int i = 0; i < y; i++) {
-			for (int j = 0; j < x; j++) {
 				if (tomatoes[i][j]==1) {
-					findRecursive(resultTomatoes, days, i, j, 0);
+					queue.add(new Tomato(j,i));
 				}
 			}
 		}
 
-		int max = 0;
+		int resultDay = bfs();
+
 		for (int i = 0; i < y; i++) {
 			for (int j = 0; j < x; j++) {
-				if (resultTomatoes[i][j]==0) {
-					bw.write("-1");
-					bw.flush();
-					br.close();
-					bw.close();
-					return ;
-				}
-				if (days[i][j] != Integer.MAX_VALUE) {
-					max = Math.max(days[i][j], max);
+//				System.out.print(tomatoes[i][j]+" ");
+				if (tomatoes[i][j]==0) {
+					resultDay = -1;
 				}
 			}
+//			System.out.println();
 		}
-		bw.write(max+"");
+
+		bw.write(String.valueOf(resultDay));
+
+
 
 
 		bw.flush();
@@ -62,30 +74,39 @@ public class Main {
 		bw.close();
 	}
 
-	public static void findRecursive(int[][] tomatoes, int[][] days, int y, int x, int day) {
-		if (tomatoes[y][x]==-1) return;
-		if (days[y][x] <= day) {
-			return;
-		}
-		days[y][x]=day;
-		tomatoes[y][x]=1;
-//		if (day>4) return;
+	private static int bfs() {
+		int xLength = tomatoes[0].length;
+		int yLength = tomatoes.length;
+		int day = 0;
+		while (!queue.isEmpty()) {
+			Tomato tomato = queue.poll();
+			int x = tomato.x;
+			int y = tomato.y;
+			day = tomato.day;
 
-		// visited가 없어서 계속 방문하고 호출해서 발생
+			if (x<xLength-1) {
+				if (tomatoes[y][x+1]==0) {
+					queue.add(new Tomato(x+1,y,day+1));
+				}
+			}
+			if (y<yLength-1) {
+				if (tomatoes[y+1][x]==0) {
+					queue.add(new Tomato(x,y+1,day+1));
+				}
+			}
+			if (x>0) {
+				if (tomatoes[y][x-1]==0) {
+					queue.add(new Tomato(x-1,y,day+1));
+				}
+			}
+			if (y>0) {
+				if (tomatoes[y-1][x]==0) {
+					queue.add(new Tomato(x,y-1,day+1));
+				}
+			}
 
-		// 지금 조건을 못탄다ㅣ...
-		//
-		if (y>0) {
-			findRecursive(tomatoes, days, y-1, x, day+1);
 		}
-		if (x>0) {
-			findRecursive(tomatoes, days, y, x-1, day+1);
-		}
-		if (x<tomatoes[0].length-1){
-			findRecursive(tomatoes, days, y, x+1, day+1);
-		}
-		if (y<tomatoes.length-1) {
-			findRecursive(tomatoes, days, y+1, x, day+1);
-		}
+		return day;
 	}
+
 }
