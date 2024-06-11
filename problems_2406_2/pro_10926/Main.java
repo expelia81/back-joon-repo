12fe2cfac37bcbfb712ac67b438d31dd7836b6bb
김창훈 <expelia81@gmail.com>
2024-06-11 13:06:48 +0900

    commit 다이나믹 프로그래밍

---
+++ problems_2406_2/pro_10926/Main.java
@@ -0,0 +1,18 @@
+package problems_2406_2.pro_10926;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write(br.readLine()+"??!");
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
